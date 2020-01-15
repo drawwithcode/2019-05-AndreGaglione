6ef99668ead5cc3ba1f71c3e56f18be492e05d78
Andrea Gaglione <a.gaglione7@gmail.com>
2020-01-15 13:13:27 +0100

    correzione

--- sketch.js
+++ sketch.js
@@ -1,49 +1,90 @@
+var spiral;
+var font;
 var capture;
 
+function preload() {
+  font = loadFont('assets/Gayathri-Bold.ttf');
+}
+
 function setup() {
-  createCanvas(windowWidth,windowHeight);
-
+  createCanvas(windowWidth, windowHeight);
+  background('black');
+  //for framecount
+  frameRate(5);
   capture = createCapture(VIDEO);
-  capture.size(windowWidth, windowHeight);
-  capture.hide();
+  capture.size(640, 480);
+  capture.hide();//la mostro dopo
 
-  mic = new p5.AudioIn();
-  mic.start();
+  button = createButton("take a pic");
+  button.position(windowWidth / 2 - 50, windowHeight / 2 + 100);
+  button.mousePressed(pic);
+
+
+
+  spiral = new Spiral(600, 150);
+
+  push();
+  rSlider = createSlider(0, 255, 0);
+  rSlider.position(windowWidth / 2 - 100, windowHeight / 2 + 30);
+  gSlider = createSlider(0, 255, 0);
+  gSlider.position(windowWidth / 2 - 100, windowHeight / 2 + 50);
+  bSlider = createSlider(0, 255, 0);
+  bSlider.position(windowWidth / 2 - 100, windowHeight / 2 + 70);
+  pop();
+
+}
+
+function pic() {
+  save("selfie.png");
 }
 
 function draw() {
-  background('black');
+  push();
+  textAlign(CENTER, CENTER);
+  fill('yellow');
+  textSize(50);
+  textFont(font);
+  text("say cheeeese", windowWidth / 2, windowHeight / 2 - 20);
+  pop();
 
-  //text part
-  var myText = "press to solve the chaos";
-  drawingContext.font = "400 120px Press Start 2P Display";
-  drawingContext.textAlign = "center";
-  fill('white');
-  text(myText, width/2, pmouseY+height/3);
-
-  var inverted_height = -height;
-  var myFeed = capture.loadPixels();
-
-  //moving pretty unlogically the camera inside the space
-  if(mouseX<width/2){
-    image(myFeed, mouseY, mouseX, windowWidth/(mouseX/20), windowHeight/(mouseY/20));
-    if(mouseIsPressed){
-      //putting cointinously randomness (a sort of animation) to the camera
-      image(myFeed, mouseY, mouseX, random()*width, random()*height);
-    }
-  }
-  //in the right part things are different
-  else {
-    if(mouseIsPressed){
-      image(myFeed, mouseX, mouseY, random()*width+width/8, random()*height + inverted_height/8);
-    }
-    else {
-      image(myFeed, mouseX, mouseY, windowWidth/(mouseY/10) + width/8, windowHeight/(mouseX/10)+ inverted_height/8);
-    }
-  }
+  spiral.display();
 
 
 }
+//webcam input
+function Spiral(_x, _y,) {
+  this.x = _x;
+  this.y = _y;
 
+  this.start = width/2;
+  this.angle = 90;
+  this.speed = 0.1;
+  this.distance = this.speed*40;
 
-//Object
+  this.move = function() {
+    this.x =  this.start; //crea una spirale - tunnel
+    this.y = height/2;
+    this.angle -= this.speed;
+    this.start = this.start - this.distance;
+  }
+
+  this.display = function() {
+    translate(width/2, height/2);
+    imageMode(CENTER);
+    var myImage = capture.loadPixels();
+    rotate((frameCount));
+    rotate(this.angle);
+
+    image(myImage, this.x, this.y, myImage.width, myImage.height);
+
+    //filtro
+    var r = rSlider.value();
+    var g = gSlider.value();
+    var b = bSlider.value();
+    noStroke();
+    fill(color(r, g, b, 75));
+    rectMode(CENTER);
+    rect(this.x, this.y, myImage.width, myImage.height);
+  }
+
+}
